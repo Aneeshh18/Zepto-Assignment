d1aaa1b4199def24153bf06611d9dec47ef40bd0
Aneesh Sahu <88325646+Aneeshh18@users.noreply.github.com>
2024-01-16 19:30:22 +0530

    Final

---
+++ src/components/Config.ts
@@ -0,0 +1,52 @@
+import { userObjType } from "./Util";
+
+export const USER_LIST:userObjType[] = [
+    {
+        name: "Marina Augustine",
+        email: "maugustine.@example.com"
+    },
+    {
+        name: "Nick Gianpolous",
+        email: "n.gianpolous@example.xom"
+    },
+    {
+        name: "Naryana Gamer",
+        email: "n.gamer@example.com"
+    },
+    {
+        name: "Anita Gros",
+        email: "a.gros@example.xom"
+    },
+    {
+        name: "Megan Smith",
+        email: "m.sumith@example.com"
+    },
+    {
+        name: "John Doe",
+        email: "j.doe@example.com"
+    },
+    {
+        name: "Alice Johnson",
+        email: "a.johnson@example.com"
+    },
+    {
+        name: "Robert Thompson",
+        email: "r.thompson@example.com"
+    },
+    {
+        name: "Emily Davis",
+        email: "e.davis@example.com"
+    },
+    {
+        name: "Christopher Lee",
+        email: "c.lee@example.com"
+    },
+    {
+        name: "Mittali Mangla",
+        email: "m.mangla@example.com"
+    },
+    {
+        name: "Nick Alexander",
+        email: "n.alexander@example.com"
+    }
+];
